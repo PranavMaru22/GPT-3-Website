8c1cb65de8cc02216ee2c3b9fc48ea79d2e9d1a5
Pranav Maru <pranavmaru22@gmail.com>
2023-02-28 18:03:33 +0530

    Finished header section

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+function Header() {
+  return (
+    <div className='header__container'>
+        <NavBar />
+    </div>
+  )
+}
+
+export default Header
