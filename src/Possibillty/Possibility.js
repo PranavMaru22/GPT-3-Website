bf78de39d43fc06e4d7cea244c5e36007ca29ee1
Pranav Maru <pranavmaru22@gmail.com>
2023-03-01 19:53:28 +0530

    fix possibility section

--- src/Possibillty/Possibility.js
+++ src/Possibillty/Possibility.js
@@ -4,21 +4,23 @@ const WOMAN = require("../gpt-3 assets/possibility.png")
 
 function Possibility() {
     return (
-        <div>
-            <div>
+        <div className='possibility__flex'>
+            <div className='possibility__container'>
                 <img 
                     src={WOMAN}
                     alt="possibility-pic"
                     className='possibility__image'
                 />
-                <div>
-                    <p>Request Early Access to Get Started</p>
-                    <h2>The possibilities are beyond your imagination</h2>
-                    <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                    <p>Request Early Access to Get Started</p>
+                <div className='possibility__content'>
+                    <div>
+                        <p className='first__para'>Request Early Access to Get Started</p>
+                        <h2>The possibilities are beyond your imagination</h2>
+                        <p className='second__para'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                        <p className='third__para'>Request Early Access to Get Started</p>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className='request__card'>
                 <div>
                     <p>Request Early Access to Get Started</p>
                     <h2>Register today & start exploring the endless possiblities.</h2>
