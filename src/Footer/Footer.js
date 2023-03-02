9f86b6ebdb5e09027db41ded8f4a05cbfc1b8198
Pranav Maru <pranavmaru22@gmail.com>
2023-03-02 11:10:23 +0530

    fixed footer section

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,31 +3,31 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <div>
-      <div>
+    <div className='footer__container'>
+      <div className='footer__heading'>
         <h1>Do you want to step in to the future before others</h1>
-        <button>Request Early Access</button>
+        <button className='request_btn'>Request Early Access</button>
       </div>
-      <div>
-        <div>
+      <div className='footer__content'>
+        <div className='footer__logo'>
           <h2>GPT-3</h2>
           <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
-        <div>
-          <div>
+        <div className='footer__list__container'>
+          <div className='footer__list__1'>
             <h3>Links</h3>
             <a href='#overons'>Overons</a>
             <a href='#social-media'>Social Media</a>
             <a href='#counters'>Counters</a>
             <a href='#contact'>Contact</a>
           </div>
-          <div>
+          <div className='footer__list__2'>
             <h3>Company</h3>
             <a href='#terms-conditions'>Terms & Conditions</a>
             <a href='#privacy'>Privacy</a>
             <a href='#contact'>Contact</a>
           </div>
-          <div>
+          <div className='footer__list__3'>
             <h3>Get In Touch</h3>
             <p>Crechterwoord K12 182 DK Alknjkcb</p>
             <p>+91-8433682500</p>
@@ -35,7 +35,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <p>© 2023 GPT-3.All rights reserved.</p>
+      <p>© 2023 GPT-3. All rights reserved.</p>
     </div>
   )
 }
